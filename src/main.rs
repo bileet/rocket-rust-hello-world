6c86d7108177d9ba427a0aec0d4874d06fb03f66
Bilal Barina <bilalbarina1@gmail.com>
2023-02-16 18:42:01 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,14 @@ fn hello() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/loaderio-8f747818022b458427567324f00e2493")]
+fn verify() -> &'static str {
+    "loaderio-8f747818022b458427567324f00e2493"
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite()
+    .mount("/", routes![hello])
+    .mount("/loaderio-8f747818022b458427567324f00e2493", routes![verify])
+    .launch();
 }
